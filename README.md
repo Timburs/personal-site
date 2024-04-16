5e2ca2dd95bfd91330417f034e7537298fb410bd
timburs <timbursgit@gmail.com>
2024-04-16 08:04:16 -0400

    some html and readme updates

---
+++ README.md
@@ -0,0 +1,4 @@
+# Site Info
+
+- Static site with a custom domain hosted on Azure Cloud using : Storage Account, Front Door, Azure DNS
+- TODO: add more details when finished hosting
---
+++ infra/main.tf
@@ -0,0 +1,3 @@
+# TODO : Import the storage account to state if possible from Free Trial subscription
+# TODO : Create CI process to push to $web container
+# TODO : When deploying, ensure that CSS and JS files have the proper content type on Azure
